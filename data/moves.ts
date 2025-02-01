19f5053cbcc46e1fb909c0dd58fe5d946ddafff0
Master Ternimus <chinmaya.kallaje7@gmail.com>
2025-02-01 11:46:42 +0530

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -3627,14 +3627,6 @@ export const Moves: { [moveid: string]: MoveData } = {
 				move.accuracy = 85;
 			}
 		},
-		onTry(source, target, move) {
-			if (source.species.name === 'Darkrai' || move.hasBounced) {
-				return;
-			}
-			this.add('-fail', source, 'move: Dark Void');
-			this.hint("Only a Pokemon whose form is Darkrai can use this move.");
-			return null;
-		},
 		noSketch: true,
 		secondary: null,
 		target: "allAdjacentFoes",
