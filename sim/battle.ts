1600209950cb3547c5de8eeb69ed20aeee08aed6
Master Ternimus <chinmaya.kallaje7@gmail.com>
2026-05-31 17:51:13 +0530

    Enforce 1 gmax per side

--- sim/battle.ts
+++ sim/battle.ts
@@ -2476,7 +2476,7 @@ export class Battle {
 	For big top
 	*/
 	StrikerBonus(source: Pokemon, move: ActiveMove): number {
-		const strikerabilities = ['guts', 'sheerforce', 'hugepower', 'purepower', 'punkrock'];
+		const strikerabilities = ['guts', 'sheerforce', 'hugepower', 'purepower', 'punkrock', 'striker'];
 		const defensiveAbilities = ['bulletproof', 'sturdy', 'heavymetal', 'thickfat', 'solidrock'];
 		const noabilityodds = [[7, 13], [6, 12, 13], [5, 11, 13], [4, 10, 13], [3, 9, 13], [2, 8, 12, 13], [1, 7, 11, 13], [0, 6, 10, 12, 13], [-1, 5, 9, 11, 13], [-1, 4, 8, 10, 13], [-1, 3, 7, 9, 13], [-1, 2, 6, 8, 13], [-1, 1, 5, 7, 13]];
 		const abilityodds = [[-1, 7, 13], [-1, -1, 13], [-1, -1, 7, 13], [-1, -1, -1, 13], [-1, -1, -1, 7, 13], [-1, -1, -1, -1, 13]];
@@ -3551,8 +3551,11 @@ export class Battle {
 			pokemon.canTerastallize = null;
 			pokemon.canMegaEvo = false;
 			pokemon.canUltraBurst = null;
-			pokemon.canDynamax = false;
 			pokemon.side.gimmickCount++;
+			if (gimmick === 'Gigantamax') {
+				pokemon.side.dynamaxUsed = true;
+				if (pokemon.side.allySide) pokemon.side.allySide.dynamaxUsed = true;
+			}
 		}
 		if (pokemon.side.gimmickCount === 2) {
 			pokemon.side.dynamaxUsed = true;
